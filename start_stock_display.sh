2be61e3504a2c62462c5d0379752c904ea6c66d1
SweetGibon <tuhinatitli@gmail.com>
2025-05-18 14:33:29 -0700

    Update start_stock_display.sh

--- start_stock_display.sh
+++ start_stock_display.sh
@@ -2,6 +2,7 @@
 sleep 30  # Wait for network connection
 
 # Set the DISPLAY environment variable for the graphical session
+## Replace /****/ with your username(under the home directory)
 export DISPLAY=:0
 export XAUTHORITY=/home/****/.Xauthority
 
